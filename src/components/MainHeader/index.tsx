5dd1c61e8c8d796c91a408bbfb7c1a8ad0e5fab7
Jacakson Farias <jackson.emiliano15@gmail.com>
2021-06-15 20:55:19 -0300

    WIP MaindHeader style

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 
-import { Container } from "./styles";
+import { Container, Profile, Welcome, UserName } from "./styles";
 
 const MainHeader: React.FC = () =>{
     return(
         <Container>
-            <h1>MainHeader</h1>
+            <h1>Toogle</h1>
+
+            <Profile>
+                <Welcome>Olá</Welcome>
+                <UserName>Jackson Alves</UserName>
+            </Profile>
         </Container>
     );
 }
