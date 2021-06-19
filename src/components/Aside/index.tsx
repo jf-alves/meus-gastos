ee51ec0978822168dd63a9df2ccebd1d45b485df
Jacakson Farias <jackson.emiliano15@gmail.com>
2021-06-18 21:07:08 -0300

    Assid icons

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,11 +1,40 @@
 import React from "react";
+import { MdDashboard, MdArrowDownward, MdArrowUpward, MdExitToApp} from 'react-icons/md';
 
-import { Container } from "./styles";
+import logoImg from "../../assets/logo.svg";
+
+
+import { Container, Header, LogImg, Title, MenuContainer, MenuItemLink } from "./styles";
 
 const Aside: React.FC = () =>{
     return(
         <Container>
-            <h1>Aside</h1>
+            <Header>
+                <LogImg src={logoImg} alt="Logo Minha Carteira"/>
+                <Title>Minha Carteira</Title>
+            </Header>
+
+            <MenuContainer>
+                <MenuItemLink href="#">
+                    <MdDashboard />
+                    Dashboard
+                </MenuItemLink>
+
+                <MenuItemLink href="#">
+                    <MdArrowUpward />
+                    Entradas
+                </MenuItemLink>
+
+                <MenuItemLink href="#">
+                    <MdArrowDownward />
+                    Saidas
+                </MenuItemLink>
+
+                <MenuItemLink href="#">
+                   <MdExitToApp />
+                    Sair
+                </MenuItemLink>
+            </MenuContainer>
         </Container>
     );
 }
