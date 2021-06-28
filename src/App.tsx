10c45862e783f29acd14639f6f8150db7e98f58f
Jacakson Farias <jackson.emiliano15@gmail.com>
2021-06-27 21:15:50 -0300

    WIP create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import {ThemeProvider} from 'styled-components'
 import GlobalStyles from "./styles/GlobalStyles";
-import Layout from "./components/Layout";
-import Dashboard from "./pages/DashBoard";
-import List from "./pages/List";
+
+import Routes from './routes';
 
 import dark from "./styles/themes/dark";
 //import light from "./styles/themes/light";
@@ -12,10 +11,7 @@ const App: React.FC = () => {
     return(
         <ThemeProvider theme={dark}>
             <GlobalStyles/>
-            <Layout>
-                <Dashboard />
-                <List />
-            </Layout>
+            <Routes/>
         </ThemeProvider>
     );
 }
---
+++ src/routes/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import App from "./app.routes";
+
+const Routes: React.FC = () => (
+   <App />
+);
+
+export default App;
